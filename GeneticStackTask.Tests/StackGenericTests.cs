9dd66298f640d674cfaa205662c8a648c0c80c53
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2021-12-07 19:36:19 +0300

    Fix some code smells.

--- GeneticStackTask.Tests/StackGenericTests.cs
+++ GeneticStackTask.Tests/StackGenericTests.cs
@@ -1,49 +1,50 @@
-using System;
-using GenericStackTask;
+﻿using System;
 using System.Linq;
+using GenericStackTask;
 using NUnit.Framework;
+
+#pragma warning disable SA1600
+#pragma warning disable CA1707
 
 namespace GeneticStackTask.Tests
 {
-    [TestFixture(new[] {12, 3, 4, Int32.MaxValue, Int32.MinValue, -12, 45, 12}, 67, 8,
-        TypeArgs = new Type[] {typeof(int)})]
-    [TestFixture(new[] {0.362, 3.0987, -198.4, -1008245.78, 9012.0001}, 0.8911, 5,
-        TypeArgs = new Type[] {typeof(double)})]
-    [TestFixture(new[] {"12", "Zero", "Test", "Hello"}, "Hello, world!", 4, TypeArgs = new Type[] {typeof(string)})]
-    [TestFixture(new[] {"12", "Zero", null, "Test", "Hello", null}, null, 6, TypeArgs = new Type[] {typeof(string)})]
-    [TestFixture(new[] {'a', '\n', '4', '5'}, '\t', 4, TypeArgs = new Type[] {typeof(char)})]
-    [TestFixture(new[] {true, false, true, true}, false, 4, TypeArgs = new Type[] {typeof(bool)})]
+    [TestFixture(new[] { 12, 3, 4, int.MaxValue, int.MinValue, -12, 45, 12 }, 67, 8, TypeArgs = new Type[] { typeof(int) })]
+    [TestFixture(new[] { 0.362, 3.0987, -198.4, -1008245.78, 9012.0001 }, 0.8911, 5, TypeArgs = new Type[] { typeof(double) })]
+    [TestFixture(new[] { "12", "Zero", "Test", "Hello" }, "Hello, world!", 4, TypeArgs = new Type[] { typeof(string) })]
+    [TestFixture(new[] { "12", "Zero", null, "Test", "Hello", null }, null, 6, TypeArgs = new Type[] { typeof(string) })]
+    [TestFixture(new[] { 'a', '\n', '4', '5' }, '\t', 4, TypeArgs = new Type[] { typeof(char) })]
+    [TestFixture(new[] { true, false, true, true }, false, 4, TypeArgs = new Type[] { typeof(bool) })]
     public class StackGenericTests<T>
     {
-        private readonly Stack<T> _stack;
-        private readonly T[] _array;
-        private readonly T _value;
-        private readonly int _initCount;
         private const int someValue = 3;
+        private readonly Stack<T> stack;
+        private readonly T[] array;
+        private readonly T value;
+        private readonly int initCount;
 
         public StackGenericTests(T[] source, T value, int count)
         {
-            _stack = new Stack<T>(source);
-            _value = value;
-            _initCount = count;
-            _array = source;
+            this.stack = new Stack<T>(source);
+            this.value = value;
+            this.initCount = count;
+            this.array = source;
         }
 
         [Test]
         [Order(0)]
         public void Ctor_BasedOnEnumerableSource()
         {
-            Assert.That(_stack.Count == _initCount);
+            Assert.That(this.stack.Count == this.initCount);
         }
 
         [Test]
         [Order(1)]
         public void Iterator_Test()
         {
-            int i = _initCount;
-            foreach (var item in _stack)
+            int i = this.initCount;
+            foreach (var item in this.stack)
             {
-                Assert.AreEqual(item, _array[--i]);
+                Assert.AreEqual(item, this.array[--i]);
             }
         }
 
@@ -51,24 +52,24 @@ namespace GeneticStackTask.Tests
         [Order(2)]
         public void ToArray_Test()
         {
-            T[] copy = _stack.ToArray(); 
-            Assert.Multiple((() =>
+            T[] copy = this.stack.ToArray();
+            Assert.Multiple(() =>
             {
-                CollectionAssert.AreEqual(_array.Reverse(), copy);
-                Assert.AreNotSame(_array, copy);
-            }));
+                CollectionAssert.AreEqual(this.array.Reverse(), copy);
+                Assert.AreNotSame(this.array, copy);
+            });
         }
 
         [Test]
         [Order(3)]
         public void Push_OneElement()
         {
-            int count = _stack.Count;
-            _stack.Push(_value);
+            int count = this.stack.Count;
+            this.stack.Push(this.value);
             Assert.Multiple(() =>
             {
-                CollectionAssert.Contains(_stack, _value);
-                Assert.That(_stack.Count == count + 1);
+                CollectionAssert.Contains(this.stack, this.value);
+                Assert.That(this.stack.Count == count + 1);
             });
         }
 
@@ -76,19 +77,19 @@ namespace GeneticStackTask.Tests
         [Order(4)]
         public void Contains_Test()
         {
-            Assert.IsTrue(_stack.Contains(_value));
+            Assert.IsTrue(this.stack.Contains(this.value));
         }
 
         [Test]
         [Order(4)]
         public void Peek_Test()
         {
-            int count = _stack.Count;
-            T actual = _stack.Peek();
+            int count = this.stack.Count;
+            T actual = this.stack.Peek();
             Assert.Multiple(() =>
             {
-                Assert.That(_stack.Count == count);
-                Assert.AreEqual(_value, actual);
+                Assert.That(this.stack.Count == count);
+                Assert.AreEqual(this.value, actual);
             });
         }
 
@@ -96,37 +97,38 @@ namespace GeneticStackTask.Tests
         [Order(5)]
         public void Push_SomeElement()
         {
-            int count = _stack.Count;
+            int count = this.stack.Count;
             for (int i = 1; i <= someValue; i++)
             {
-                _stack.Push(_value);
+                this.stack.Push(this.value);
             }
 
-            Assert.That(_stack.Count == count + someValue);
+            Assert.That(this.stack.Count == count + someValue);
         }
 
         [Test]
         [Order(6)]
         public void Pop_SomeElement()
         {
-            int count = _stack.Count;
+            int count = this.stack.Count;
             for (int i = 1; i <= someValue; i++)
             {
-                T item = _stack.Pop();
+                T item = this.stack.Pop();
             }
 
-            Assert.That(_stack.Count == count - someValue);
+            Assert.That(this.stack.Count == count - someValue);
         }
 
         [Test]
         [Order(7)]
         public void Iterator_PopFromStack_Throw_InvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() =>
+            Assert.Throws<InvalidOperationException>(
+                () =>
             {
-                foreach (var item in _stack)
+                foreach (var item in this.stack)
                 {
-                    T t = _stack.Pop();
+                    T t = this.stack.Pop();
                 }
             }, "Stack cannot be changed during iteration.");
         }
@@ -135,11 +137,12 @@ namespace GeneticStackTask.Tests
         [Order(8)]
         public void Iterator_PushIntoStack_Throw_InvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() =>
+            Assert.Throws<InvalidOperationException>(
+                () =>
             {
-                foreach (var item in _stack)
+                foreach (var item in this.stack)
                 {
-                    _stack.Push(_value);
+                    this.stack.Push(this.value);
                 }
             }, "Stack cannot be changed during iteration.");
         }
@@ -148,15 +151,15 @@ namespace GeneticStackTask.Tests
         [Order(9)]
         public void Clear_Test()
         {
-            _stack.Clear();
-            Assert.That(_stack.Count == 0);
+            this.stack.Clear();
+            Assert.That(this.stack.Count == 0);
         }
 
         [Test]
         [Order(10)]
         public void Pop_StackIsEmpty_ThrowInvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() => _stack.Pop(), "Invalid operation pop, stack is empty.");
+            Assert.Throws<InvalidOperationException>(() => this.stack.Pop(), "Invalid operation pop, stack is empty.");
         }
     }
 }

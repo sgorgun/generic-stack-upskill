9a7bb5e56ca8ec6c85a17a182fb48ed830a021e0
Anzhelika Kravchuk <anzhelika_kravchuk@epam.com>
2023-10-07 05:49:03 +0000

    Update StackTests.cs

--- GeneticStackTask.Tests/StackTests.cs
+++ GeneticStackTask.Tests/StackTests.cs
@@ -1,162 +1,170 @@
-﻿using System;
+using System;
 using System.Linq;
+using GenericStackTask;
 using NUnit.Framework;
 
-namespace GenericStackTask.Tests
+namespace GeneticStackTask.Tests;
+
+[TestFixture(new[] { 12, 3, 4, int.MaxValue, int.MinValue, 12, 45, -12 }, 8, -12, 45, TypeArgs = new Type[] { typeof(int) })]
+[TestFixture(new[] { 0.362, 3.0987, -198.4, -1008245.78, 9012.0001, 0.891 }, 6, 0.891, -198.4, TypeArgs = new Type[] { typeof(double) })]
+[TestFixture(new[] { "12", "Zero", "Test", "Hello", "Hello, world!" }, 5, "Hello, world!", "Zero", TypeArgs = new Type[] { typeof(string) })]
+[TestFixture(new[] { "12", "Zero", null, "Test", "Hello", null }, 6, null, null, TypeArgs = new Type[] { typeof(string) })]
+[TestFixture(new[] { 'a', '\n', '4', '5', '\t' }, 5, '\t', '\n', TypeArgs = new Type[] { typeof(char) })]
+[TestFixture(new[] { true, false, true, false }, 4, false, true, TypeArgs = new Type[] { typeof(bool) })]
+public class StackTests<T>
 {
-    [TestFixture(new[] { 12, 3, 4, int.MaxValue, int.MinValue, -12, 45, 12 }, 67, 8, TypeArgs = new Type[] { typeof(int) })]
-    [TestFixture(new[] { 0.362, 3.0987, -198.4, -1008245.78, 9012.0001 }, 0.8911, 5, TypeArgs = new Type[] { typeof(double) })]
-    [TestFixture(new[] { "12", "Zero", "Test", "Hello" }, "Hello, world!", 4, TypeArgs = new Type[] { typeof(string) })]
-    [TestFixture(new[] { "12", "Zero", null, "Test", "Hello", null }, null, 6, TypeArgs = new Type[] { typeof(string) })]
-    [TestFixture(new[] { 'a', '\n', '4', '5' }, '\t', 4, TypeArgs = new Type[] { typeof(char) })]
-    [TestFixture(new[] { true, false, true, true }, false, 4, TypeArgs = new Type[] { typeof(bool) })]
-    [TestFixture(new int[] { }, 3, 0, TypeArgs = new Type[] { typeof(int) })]
-    public class StackTests<T>
+    private const int someValue = 3;
+    private readonly T[] setup;
+    private readonly T peek;
+    private readonly T value;
+    private readonly int initCount;
+    private Stack<T> stack;
+
+    public StackTests(T[] source, int count, T peek, T value)
     {
-        private const int someValue = 3;
-        private readonly Stack<T> stack;
-        private readonly T[] array;
-        private readonly T value;
-        private readonly int initCount;
+        this.initCount = count;
+        this.setup = new T[source.Length];
+        Array.Copy(source, this.setup, this.setup.Length);
+        this.peek = peek;
+        this.value = value;
+    }
 
-        public StackTests(T[] source, T value, int count)
+    [SetUp]
+    public void Init()
+    {
+        this.stack = new Stack<T>(this.setup);
+    }
+
+    [Test]
+    public void Ctor_BasedOnEnumerableSource()
+    {
+        Assert.That(this.stack.Count == this.setup.Length);
+        CollectionAssert.AreEqual(this.stack.ToArray(), this.setup.Reverse());
+    }
+
+    [Test]
+
+    public void Iterator_Test()
+    {
+        int i = this.initCount;
+        foreach (var item in this.stack)
         {
-            this.stack = new Stack<T>(source);
-            this.value = value;
-            this.initCount = count;
-            this.array = source;
+            Assert.AreEqual(item, this.setup[--i]);
         }
+    }
 
-        [Test]
-        [Order(0)]
-        public void Ctor_BasedOnEnumerableSource()
+    [Test]
+    public void ToArray_Test()
+    {
+        T[] copy = this.stack.ToArray();
+        Assert.Multiple(() =>
         {
-            Assert.That(this.stack.Count == this.initCount);
-        }
+            CollectionAssert.AreEqual(this.setup.Reverse(), copy);
+            Assert.AreNotSame(this.setup, copy);
+        });
+    }
 
-        [Test]
-        [Order(1)]
-        public void Iterator_Test()
+    [Test]
+
+    public void Push_OneElement()
+    {
+        int count = this.stack.Count;
+        this.stack.Push(this.value);
+        Assert.Multiple(() =>
         {
-            int i = this.initCount;
-            foreach (var item in this.stack)
-            {
-                Assert.AreEqual(item, this.array[--i]);
-            }
-        }
+            CollectionAssert.Contains(this.stack, this.value);
+            Assert.That(this.stack.Count == count + 1);
+        });
+    }
 
-        [Test]
-        [Order(2)]
-        public void ToArray_Test()
-        {
-            T[] copy = this.stack.ToArray();
-            Assert.Multiple(() =>
-            {
-                CollectionAssert.AreEqual(this.array.Reverse(), copy);
-                Assert.AreNotSame(this.array, copy);
-            });
-        }
-
-        [Test]
-        [Order(3)]
-        public void Push_OneElement()
-        {
-            int count = this.stack.Count;
-            this.stack.Push(this.value);
-            Assert.Multiple(() =>
-            {
-                CollectionAssert.Contains(this.stack, this.value);
-                Assert.That(this.stack.Count == count + 1);
-            });
-        }
-
-        [Test]
-        [Order(4)]
-        public void Contains_Test()
+    [Test]
+    public void Contains_Test()
+    {
+        if (this.stack.Count != 0)
         {
             Assert.IsTrue(this.stack.Contains(this.value));
         }
-
-        [Test]
-        [Order(4)]
-        public void Peek_Test()
+        else
         {
-            int count = this.stack.Count;
-            T actual = this.stack.Peek();
-            Assert.Multiple(() =>
-            {
-                Assert.That(this.stack.Count == count);
-                Assert.AreEqual(this.value, actual);
-            });
+            Assert.IsFalse(this.stack.Contains(this.value));
+        }
+    }
+
+    [Test]
+    public void Peek_Test()
+    {
+        int count = this.stack.Count;
+        T actual = this.stack.Peek();
+        Assert.Multiple(() =>
+        {
+            Assert.That(this.stack.Count == count);
+            Assert.AreEqual(this.peek, actual);
+        });
+    }
+
+    [Test]
+    public void Push_MoreOneElements()
+    {
+        int count = this.stack.Count;
+        for (int i = 1; i <= someValue; i++)
+        {
+            this.stack.Push(this.value);
         }
 
-        [Test]
-        [Order(5)]
-        public void Push_SomeElement()
-        {
-            int count = this.stack.Count;
-            for (int i = 1; i <= someValue; i++)
-            {
-                this.stack.Push(this.value);
-            }
+        Assert.That(this.stack.Count == count + someValue);
+    }
 
-            Assert.That(this.stack.Count == count + someValue);
+    [Test]
+    public void Pop_MoreOneElements()
+    {
+        int count = this.stack.Count;
+
+        for (int i = 1; i <= someValue; i++)
+        {
+            _ = this.stack.Pop();
         }
 
-        [Test]
-        [Order(6)]
-        public void Pop_SomeElement()
-        {
-            int count = this.stack.Count;
-            for (int i = 1; i <= someValue; i++)
-            {
-                T item = this.stack.Pop();
-            }
+        Assert.That(this.stack.Count == count - someValue);
+    }
 
-            Assert.That(this.stack.Count == count - someValue);
-        }
-
-        [Test]
-        [Order(7)]
-        public void Iterator_PopFromStack_Throw_InvalidOperationException()
-        {
-            Assert.Throws<InvalidOperationException>(
-                () =>
+    [Test]
+    public void Iterator_PopFromStack_Throw_InvalidOperationException()
+    {
+        _ = Assert.Throws<InvalidOperationException>(
+            () =>
             {
                 foreach (var item in this.stack)
                 {
-                    T t = this.stack.Pop();
+                    _ = this.stack.Pop();
                 }
             }, "Stack cannot be changed during iteration.");
-        }
+    }
 
-        [Test]
-        [Order(8)]
-        public void Iterator_PushIntoStack_Throw_InvalidOperationException()
-        {
-            Assert.Throws<InvalidOperationException>(
-                () =>
+    [Test]
+    public void Iterator_PushIntoStack_Throw_InvalidOperationException()
+    {
+        Assert.Throws<InvalidOperationException>(
+            () =>
             {
                 foreach (var item in this.stack)
                 {
                     this.stack.Push(this.value);
                 }
             }, "Stack cannot be changed during iteration.");
-        }
+    }
 
-        [Test]
-        [Order(9)]
-        public void Clear_Test()
-        {
-            this.stack.Clear();
-            Assert.That(this.stack.Count == 0);
-        }
+    [Test]
+    public void Clear_Test()
+    {
+        this.stack.Clear();
+        Assert.That(this.stack.Count == 0);
+        CollectionAssert.IsEmpty(this.stack.ToArray());
+    }
 
-        [Test]
-        [Order(10)]
-        public void Pop_StackIsEmpty_ThrowInvalidOperationException()
-        {
-            Assert.Throws<InvalidOperationException>(() => this.stack.Pop(), "Invalid operation pop, stack is empty.");
-        }
+    [Test]
+    public void Pop_StackIsEmpty_ThrowInvalidOperationException()
+    {
+        this.stack = new Stack<T>(0);
+        Assert.Throws<InvalidOperationException>(() => this.stack.Pop(), "Invalid operation pop, stack is empty.");
     }
 }
